93220c303e7ee8287e4049f8c7d6ee06c62ae285
ginger cea <gingercea@gmail.com>
2023-07-14 19:37:31 -0400

    Ejercicio 12 avances dentro de la edad  creando validadores

--- app/src/main/java/com/example/menudo/Ejercicio 12.kt
+++ app/src/main/java/com/example/menudo/Ejercicio 12.kt
@@ -1,12 +1,11 @@
 package com.example.menudo
-
-
 fun main(){
     var usuarios = mutableListOf<Usuario>()
     println("ingrese cantidad usuarios")
     val cantidadUsuarios = readln().toInt()
     for( i in 1..cantidadUsuarios){
         println("ingresar nombre ")
+
         var nombre = readln()
         while ( validarNombre(nombre)){
             println("nombre invalido, ingresar nombre valido")
@@ -14,42 +13,57 @@ fun main(){
         }
     
         var apellido = readln().toString()
-        validarApellido(apellido)
+        while ( validarApellido(apellido)) {
+            println("apellido invalido, ingresar apellido valido")
+            apellido = readln()
+        }
+
         var edad = readln().toInt()
-        validarEdad(edad)
+        while ( validarEdad(edad)) {
+            println("edad invalida, ingresar edad valido")
+            edad = readln().toInt()
+        }
+
+
         var correo = readln()
-        validarCorreo(correo)
+        while ( validarCorreo(correo) {
+            println("correo invalido, ingresar correo valido")
+            correo = readln()
+
         
         var sistemaSalud =readln()
         validarsistemaSalud(sistemaSalud)
         var usuario=  Usuario(nombre, apellido, edad, correo, sistemaSalud)
         usuarios.add(usuario)
     }
-    for(u in usuarios){
-     println(u)
+
+    for(u in usuarios.startedBy {
+        usuario -> usuario.edad
+    }){
+     println($u)
 
        }
 
 }
-
 fun validarsistemaSalud(sistemaSalud: String) {
-
+return sistemaSalud=="Fonasa"
 }
 
 fun validarCorreo(correo: String) {
-
+    return
 }
 
-fun validarEdad(edad: Int) {
-
+fun validarEdad(edad: Int):Boolean {
+    return
 }
 
 fun validarApellido(apellido: String) {
-
+   return
 }
 
 fun validarNombre(nombre: String) :Boolean {
  return nombre.length in 1..20 
+
 }
 
-data class Usuario(var nombre:String,var apellido:String,var edad: Int, var correo:String,var sistemaSalud:String)
+data class Usuario(var nombre:String,var apellido:String,val edad: Int, var correo:String,var sistemaSalud:String)
